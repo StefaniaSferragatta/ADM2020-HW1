6af9cfa265fd2046385f2cca312edef823ee92e7
Stefania Sferragatta <41596224+StefaniaSferragatta@users.noreply.github.com>
2020-10-19 11:18:12 +0200

    Update Time_Delta.py

--- HACKERRANK_Date&Time/Time_Delta.py
+++ HACKERRANK_Date&Time/Time_Delta.py
@@ -1,3 +1,4 @@
+#!/bin/python3
 
 import math
 import os
@@ -6,20 +7,27 @@ import re
 import sys
 from datetime import datetime
 
+# Complete the time_delta function below.
 def time_delta(t1, t2):
-    format_code = '%a %d %B %Y %H:%M:%S %z' #the format code, 'nameday numday month year hour:mins:sec timezone'
+    format_code = '%a %d %b %Y %H:%M:%S %z' #the format code, 'nameday numday month year hour:mins:sec timezone'
     t1 = datetime.strptime(t1,format_code) #create a datetime object from the given string (using the format code)
     t2 = datetime.strptime(t2,format_code)
-    diff_sec = int((t1-t2).total_seconds()) #returns the difference of the dates t1 and t2 in seconds
+    diff_sec = int(abs((t1-t2)).total_seconds()) #returns the difference of the dates t1 and t2 in seconds
     output = str(diff_sec) #convert it in string
     return output
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
     t = int(input())
+
     for t_itr in range(t):
         t1 = input()
+
         t2 = input()
+
         delta = time_delta(t1, t2)
+
         fptr.write(delta + '\n')
+
     fptr.close()
